7fa2daabcee85a6c45875fe8fc4d3ba05a8bb424
Jacob Fox <jacob@electricquilt.com>
2022-08-15 12:08:36 -0400

    Add project files.

---
+++ DebuggingBugRepro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DebuggingBugRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DebuggingBugRepro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DebuggingBugRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
